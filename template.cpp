6b3a85c9367476611d02b4058032f80c375789bb
Dave Fol <dof5@cornell.edu>
2022-03-11 01:36:41 -0500

    add a competitive programming template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <cstdint>
+#include <math.h>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
